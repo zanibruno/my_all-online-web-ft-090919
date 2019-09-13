7860affa1facb05aa0bfb6ebd938b799e978ee1a
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-13 19:16:56 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   return_array = []
 counter = 0 
 while counter < collection.length  
-  yield(collection[counter])
+  return_array << yield(collection[counter])
   counter += 1
 end
 end
